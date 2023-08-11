4325d1677f564b3b585acad67a835055382fd4c4
Azharuddin <azhargiri@gmail.com>
2023-08-11 10:31:46 +0700

    update

--- sik/rumah-core/app/Http/Controllers/HomeController.php
+++ sik/rumah-core/app/Http/Controllers/HomeController.php
@@ -3,16 +3,22 @@
 namespace App\Http\Controllers;
 
 use App\Models\Innovation;
+use App\Models\Training;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+	protected $innovationLimit = 3;
+	protected $trainingLimit = 3;
+
     public function showHome()
     {
-        $data_inovasi = Innovation::whereNotNull('photo')->latest('id')->limit(3)->get();
+        $data_inovasi = Innovation::whereNotNull('photo')->latest('id')->limit($this->innovationLimit)->get();
+		$trainings = Training::orderBy('created_at', 'desc')->limit($this->trainingLimit)->get();
 
         return view('home/home', [
             'innovations' => $data_inovasi,
+			'trainings' => $trainings,
         ]);
     }
 }
